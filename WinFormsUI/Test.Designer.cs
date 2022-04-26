b2a6a3661d8c17de6ceb8ea4e73b225ca0db29d7
Samet Furkan Kopar <sametkopar465@gmail.com>
2022-04-27 01:04:00 +0300

    sınav uygulaması algoritması oluşturuldu

--- WinFormsUI/Test.Designer.cs
+++ WinFormsUI/Test.Designer.cs
@@ -29,12 +29,98 @@ namespace WinFormsUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_A = new System.Windows.Forms.Button();
+            this.btn_B = new System.Windows.Forms.Button();
+            this.btn_C = new System.Windows.Forms.Button();
+            this.btn_D = new System.Windows.Forms.Button();
+            this.lbl_QuestionText = new System.Windows.Forms.Label();
+            this.btn_AskQuestion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_A
+            // 
+            this.btn_A.Location = new System.Drawing.Point(43, 211);
+            this.btn_A.Name = "btn_A";
+            this.btn_A.Size = new System.Drawing.Size(117, 51);
+            this.btn_A.TabIndex = 0;
+            this.btn_A.Text = "A";
+            this.btn_A.UseVisualStyleBackColor = true;
+            // 
+            // btn_B
+            // 
+            this.btn_B.Location = new System.Drawing.Point(197, 211);
+            this.btn_B.Name = "btn_B";
+            this.btn_B.Size = new System.Drawing.Size(102, 51);
+            this.btn_B.TabIndex = 1;
+            this.btn_B.Text = "B";
+            this.btn_B.UseVisualStyleBackColor = true;
+            this.btn_B.Click += new System.EventHandler(this.btn_B_Click);
+            // 
+            // btn_C
+            // 
+            this.btn_C.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_C.Location = new System.Drawing.Point(360, 211);
+            this.btn_C.Name = "btn_C";
+            this.btn_C.Size = new System.Drawing.Size(154, 51);
+            this.btn_C.TabIndex = 2;
+            this.btn_C.Text = "C";
+            this.btn_C.UseVisualStyleBackColor = true;
+            this.btn_C.Click += new System.EventHandler(this.btn_C_Click);
+            // 
+            // btn_D
+            // 
+            this.btn_D.Location = new System.Drawing.Point(554, 211);
+            this.btn_D.Name = "btn_D";
+            this.btn_D.Size = new System.Drawing.Size(161, 51);
+            this.btn_D.TabIndex = 3;
+            this.btn_D.Text = "D";
+            this.btn_D.UseVisualStyleBackColor = true;
+            this.btn_D.Click += new System.EventHandler(this.btn_D_Click);
+            // 
+            // lbl_QuestionText
+            // 
+            this.lbl_QuestionText.AutoSize = true;
+            this.lbl_QuestionText.Location = new System.Drawing.Point(224, 84);
+            this.lbl_QuestionText.Name = "lbl_QuestionText";
+            this.lbl_QuestionText.Size = new System.Drawing.Size(50, 20);
+            this.lbl_QuestionText.TabIndex = 4;
+            this.lbl_QuestionText.Text = "label1";
+            // 
+            // btn_AskQuestion
+            // 
+            this.btn_AskQuestion.Location = new System.Drawing.Point(270, 348);
+            this.btn_AskQuestion.Name = "btn_AskQuestion";
+            this.btn_AskQuestion.Size = new System.Drawing.Size(244, 62);
+            this.btn_AskQuestion.TabIndex = 5;
+            this.btn_AskQuestion.Text = "Sınava Basla";
+            this.btn_AskQuestion.UseVisualStyleBackColor = true;
+            this.btn_AskQuestion.Click += new System.EventHandler(this.btn_AskQuestion_Click);
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(889, 579);
+            this.Controls.Add(this.btn_AskQuestion);
+            this.Controls.Add(this.lbl_QuestionText);
+            this.Controls.Add(this.btn_D);
+            this.Controls.Add(this.btn_C);
+            this.Controls.Add(this.btn_B);
+            this.Controls.Add(this.btn_A);
+            this.Name = "Test";
             this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_A;
+        private System.Windows.Forms.Button btn_B;
+        private System.Windows.Forms.Button btn_C;
+        private System.Windows.Forms.Button btn_D;
+        private System.Windows.Forms.Label lbl_QuestionText;
+        private System.Windows.Forms.Button btn_AskQuestion;
     }
 }
--- WinFormsUI/Test.cs
+++ WinFormsUI/Test.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Businiess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,11 +14,351 @@ namespace WinFormsUI
 {
     public partial class Test : Form
     {
-        public Test()
-        {
-            InitializeComponent();
-        }
+
+        StudentAnswerManager studentsAnswersManager = new StudentAnswerManager(new EfStudentAnswerDal());
+        QuestionManager questionManager = new QuestionManager(new EfQuestionDal());
+        UserManager userManager = new UserManager(new EfUserDal());
+        Random random = new Random();
+        List<int> randValue = new List<int>();
+        List<int> studentQuestionId = new List<int>();
+        int NumberOfQuestionsToBeAsked = 1;
+        int questionIndex = 0;
+        int questionNumberToBeAskedForTheFirstTime = 0;
         public string userName { get; set; }
         public string password { get; set; }
+        public void btn_AskQuestion_Click(object sender, EventArgs e)
+        {
+            btn_A.Visible = true;
+            btn_B.Visible = true;
+            btn_C.Visible = true;
+            btn_D.Visible = true;
+            btn_A.Enabled = true;
+            btn_B.Enabled = true;
+            btn_C.Enabled = true;
+            btn_D.Enabled = true;
+
+            var studentId = userManager.GetUserWithUserNameAndPassword(userName, password).Data.Id;
+            var studentFirstLoginDate = userManager.GetUserWithUserNameAndPassword(userName, password).Data.FirstLoginDate;
+            //int studentQuestionsCount = studentsAnswersManager.GetAllStudentAnswerWithStudentId(studentId).Data.Count();
+            if (DateTime.Now.Day == studentFirstLoginDate.Day + 1 ||
+                DateTime.Now.Day == studentFirstLoginDate.Day + 7 ||
+                DateTime.Now.Day == studentFirstLoginDate.Day + 30 ||
+                DateTime.Now.Day == studentFirstLoginDate.Day + 90 ||
+                DateTime.Now.Day == studentFirstLoginDate.Day + 365)
+            {
+                foreach (var item in studentsAnswersManager.GetAllStudentAnswerWithStudentId(studentId).Data)
+                {
+                    if (questionIndex == 0)
+                    {
+                        studentQuestionId.Add(item.QuestionId);
+                    }
+
+                }
+                lbl_QuestionText.Text = studentsAnswersManager.GetStudentQuestionWithStudentIdAndQuestionId(studentId, studentQuestionId[questionIndex]).Data.QuestionText;
+                btn_A.Text = questionManager.GetQuestionsById(studentQuestionId[questionIndex]).Data.AnswerA;
+                btn_B.Text = questionManager.GetQuestionsById(studentQuestionId[questionIndex]).Data.AnswerB;
+                btn_C.Text = questionManager.GetQuestionsById(studentQuestionId[questionIndex]).Data.AnswerC;
+                btn_D.Text = questionManager.GetQuestionsById(studentQuestionId[questionIndex]).Data.AnswerD;
+                if (questionIndex < studentQuestionId.Count())
+                {
+                    questionIndex++;
+                }
+
+            }
+
+
+            else
+            {
+                //eger sayı gelmesse sonsuz döngü olur!!
+                studentQuestionId.Clear();
+                int questionID = random.Next(1, 10);
+                if (questionManager.GetAll().Data.Count() -
+                studentsAnswersManager.GetAllStudentAnswerWithStudentId(studentId).Data.Count()
+                < NumberOfQuestionsToBeAsked)
+                     NumberOfQuestionsToBeAsked = questionManager.GetAll().Data.Count() -
+                     studentsAnswersManager.GetAllStudentAnswerWithStudentId(studentId).Data.Count();
+                //bu kod sorulacak soru sayısı tabloda yoksa olan kadarını sormaya yarar
+                if (NumberOfQuestionsToBeAsked==0)
+                {
+                    MessageBox.Show("Sınav bitti");
+                    this.Hide();
+                }
+                while (NumberOfQuestionsToBeAsked != 0)
+                {
+                    if (studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, questionID).Success == false
+                        && randValue.Contains(questionID) == false)//soru onceden sorulmamıssa
+                    {
+
+                        studentQuestionId.Add(questionID);
+                        lbl_QuestionText.Text = studentsAnswersManager.GetStudentQuestionWithStudentIdAndQuestionId(studentId, studentQuestionId[questionID]).Data.QuestionText;
+                        btn_A.Text = questionManager.GetQuestionsById(studentQuestionId[questionID]).Data.AnswerA;
+                        btn_B.Text = questionManager.GetQuestionsById(studentQuestionId[questionID]).Data.AnswerB;
+                        btn_C.Text = questionManager.GetQuestionsById(studentQuestionId[questionID]).Data.AnswerC;
+                        btn_C.Text = questionManager.GetQuestionsById(studentQuestionId[questionID]).Data.AnswerD;
+                        NumberOfQuestionsToBeAsked--;
+                        studentsAnswersManager.Add(new StudentAnswer
+                        {
+                            QuestionId = studentQuestionId[questionID],
+                            StudentId = studentId,
+                            Validation = false,
+                            SigmaCount = 0
+                        });
+                        //if (numberofquestionstobeasked == 0)
+                        //{
+                        //    break;
+                        //}
+
+                    }
+                    //cok onemli hata PATLIYOR!!!
+                    // HATA: ilk soru sorulur sonraki soru daha ilk soru gözükmeden ekrana yazılır.
+                    // diğer soruyu soracak zaman bazlı olabilir 
+                    //cozuldu gibi
+
+                    else
+                    {
+                        randValue.Add(questionID);
+                        Random random = new Random();
+                        questionID = random.Next(2, 7);
+                    }
+
+                }   
+
+            }
+
+        }
+        
+        public void btn_A_Click(object sender, EventArgs e)
+        {
+            btn_A.Enabled = false;
+            btn_B.Enabled = false;
+            btn_C.Enabled = false;
+            btn_D.Enabled = false;
+            //------verilen cevabı tabloya atma
+            var studentId = userManager.GetUserWithUserNameAndPassword(userName, password).Data.Id;
+            StudentAnswer temp = null;
+
+            if (questionIndex + 1 < studentQuestionId.Count())//sigmadan geldiyse
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionIndex]).Data;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.AnswerA ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+
+
+            }
+            else //soru ilk defa soruluyorsa
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data;
+
+                questionNumberToBeAskedForTheFirstTime++;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.AnswerA ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                        // sigma tamamlanmakodları
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+            }
+            studentsAnswersManager.Update(temp);
+
+
+        }
+
+        private void btn_B_Click(object sender, EventArgs e)
+        {
+            btn_A.Enabled = false;
+            btn_B.Enabled = false;
+            btn_C.Enabled = false;
+            btn_D.Enabled = false;
+            //------verilen cevabı tabloya atma
+            var studentId = userManager.GetUserWithUserNameAndPassword(userName, password).Data.Id;
+            StudentAnswer temp = null;
+
+            if (questionIndex + 1 < studentQuestionId.Count())//sigmadan geldiyse
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionIndex]).Data;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.AnswerB ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+
+
+            }
+            else //soru ilk defa soruluyorsa
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data;
+
+                questionNumberToBeAskedForTheFirstTime++;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.AnswerB ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                        // sigma tamamlanmakodları
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+            }
+            studentsAnswersManager.Update(temp);
+        }
+
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            btn_A.Enabled = false;
+            btn_B.Enabled = false;
+            btn_C.Enabled = false;
+            btn_D.Enabled = false;
+            //------verilen cevabı tabloya atma
+            var studentId = userManager.GetUserWithUserNameAndPassword(userName, password).Data.Id;
+            StudentAnswer temp = null;
+
+            if (questionIndex + 1 < studentQuestionId.Count())//sigmadan geldiyse
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionIndex]).Data;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.AnswerC ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+
+
+            }
+            else //soru ilk defa soruluyorsa
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data;
+
+                questionNumberToBeAskedForTheFirstTime++;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.AnswerC ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                        // sigma tamamlanmakodları
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+            }
+            studentsAnswersManager.Update(temp);
+        }
+
+        private void btn_D_Click(object sender, EventArgs e)
+        {
+            btn_A.Enabled = false;
+            btn_B.Enabled = false;
+            btn_C.Enabled = false;
+            btn_D.Enabled = false;
+            //------verilen cevabı tabloya atma
+            var studentId = userManager.GetUserWithUserNameAndPassword(userName, password).Data.Id;
+            StudentAnswer temp = null;
+
+            if (questionIndex + 1 < studentQuestionId.Count())//sigmadan geldiyse
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionIndex]).Data;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.AnswerD ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionIndex - 1]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+
+
+            }
+            else //soru ilk defa soruluyorsa
+            {
+                temp = studentsAnswersManager.GetStudentAnswerWithStudentIdAndQuestionId(studentId, studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data;
+
+                questionNumberToBeAskedForTheFirstTime++;
+                if (questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.AnswerD ==
+                    questionManager.GetQuestionsById(studentQuestionId[questionNumberToBeAskedForTheFirstTime]).Data.CorrectAnswer)//cevap dogruysa
+                {
+                    temp.Validation = true;
+                    temp.SigmaCount++;
+                    if (temp.SigmaCount == 6)
+                    {
+                        MessageBox.Show("sigma tamamlandı");
+                        // sigma tamamlanmakodları
+                    }
+
+                }
+                else
+                {
+                    temp.Validation = false;
+                    temp.SigmaCount = 0;
+                }
+            }
+            studentsAnswersManager.Update(temp);
+        }
     }
 }
